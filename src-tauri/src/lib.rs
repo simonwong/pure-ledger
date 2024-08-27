77fc6ab300f57d167d06cace245888fca1ff5c0b
Simon <wsj_simon@163.com>
2024-08-27 22:41:55 +0800

    feat: add updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,12 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build());
+            Ok(())
+        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
