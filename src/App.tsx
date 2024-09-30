cb5e26d85ede8296680827e25bec5ce795d4d96f
Simon <wsj_simon@163.com>
2024-09-30 14:42:17 +0800

    feat: 优化初始化加载渲染

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import LedgerMenu from "@/components/LedgerMenu";
 import BillPage from "@/components/BillPage";
 import { ModalHost } from "@easy-shadcn/react";
+import { Loader2Icon } from "lucide-react";
+import { useQueryLedgers } from "./store/ledger";
 
 import "./global.css";
 
 function App() {
+  const { data: ledgerList, isLoading } = useQueryLedgers();
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <Loader2Icon className="animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="flex">
         <div className="p-8">
-          <LedgerMenu />
+          <LedgerMenu ledgerList={ledgerList} />
         </div>
         <div className="flex-1 p-8">
           <BillPage />
