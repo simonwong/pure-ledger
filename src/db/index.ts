e86d89b258e546cd5a5d3454b59bba1bea9c36cc
Simon <wsj_simon@163.com>
2025-01-09 11:21:12 +0800

    refactor: update DevTool component and database seed handling
    - Replaced the old seed functions with new implementations in `src/db/seed.ts` for better data management.
    - Updated the `DevTool` component in `src/components/DevTool/index.tsx` to use the new seed functions for generating preview and test data.
    - Removed the obsolete `seeds.ts` file to streamline the codebase.
    - Refactored database connection handling in `src/db/index.ts` to improve clarity and maintainability.

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,35 +2,44 @@ import Database from '@tauri-apps/plugin-sql';
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
 import * as schema from './schema';
 
-const sqliteDB = await Database.load('sqlite:ledger.db');
-// const sqliteDB = await Database.load('sqlite:test.db');
+// const sqliteDB = await Database.load('sqlite:ledger.db');
+const sqliteDB = await Database.load('sqlite:test.db');
 
 // 参考：https://github.com/tdwesten/tauri-drizzle-sqlite-proxy-demo/blob/main/src/db/database.ts
 
-export const db = drizzle(
-  async (sql, params, method) => {
-    try {
-      if (sql.startsWith('select')) {
-        const selectRes = await sqliteDB.select<unknown[]>(sql, params).catch((e) => {
-          console.error(e);
-          return [];
-        });
-        const rowsData = selectRes.map((row: any) => {
-          return Object.values(row);
-        });
+export const getDrizzleSqliteDB = (tauriSqliteDB: Database) =>
+  drizzle(
+    async (sql, params, method) => {
+      try {
+        if (sql.startsWith('select')) {
+          const selectRes = await tauriSqliteDB.select<unknown[]>(sql, params).catch((e) => {
+            console.error(e);
+            return [];
+          });
+          const rowsData = selectRes.map((row: any) => {
+            return Object.values(row);
+          });
 
-        return {
-          rows: method === 'all' ? rowsData : rowsData[0],
-        };
+          return {
+            rows: method === 'all' ? rowsData : rowsData[0],
+          };
+        }
+        const res = await tauriSqliteDB.execute(sql, params);
+        return { rows: [], ...res };
+      } catch (e) {
+        console.error(
+          'Error from sqlite: ',
+          e instanceof Error ? e.message : e,
+          sql,
+          params,
+          method
+        );
+        return { rows: [] };
       }
-      const res = await sqliteDB.execute(sql, params);
-      return { rows: [], ...res };
-    } catch (e) {
-      console.error('Error from sqlite: ', e instanceof Error ? e.message : e, sql, params, method);
-      return { rows: [] };
+    },
+    {
+      schema,
     }
-  },
-  {
-    schema,
-  }
-);
+  );
+
+export const db = getDrizzleSqliteDB(sqliteDB);
